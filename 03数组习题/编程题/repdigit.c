8358e44f30c2c3fcd8b8a6d75f03a47920e66931
liushangyang <liu3469449669@hotmail.com>
2024-09-22 15:22:52 +0800

    测试

--- 03数组习题/编程题/repdigit.c
+++ 03数组习题/编程题/repdigit.c
@@ -2,44 +2,59 @@
 #include<stdio.h>
 
 int main(void){
-    bool digit_seen[10]={false};
-    bool digit_repeat[10]={false};
+    
+    int  digit_repeat[10]={0};
+    int digit_num[10]={0,1,2,3,4,5,6,7,8,9};
     int digit;
     long n;
     int i=0;
     int m=0;
+    int t=0;
+
+    for(;;){
 
     printf("Enter a number: ");
     scanf("%ld",&n);
+
+    if (n==0)
+    {
+        break;
+    }
+    
 
     while (n>0)
     {
         /* code */
         digit=n%10;
-        if(digit_seen[digit])
-           digit_repeat[digit]=true;
-        digit_seen[digit]=true;
+     
+       
+        digit_repeat[digit]++;
         n/=10;
     }
-  while (i<10)
-  {
-    /* code */
-    if (digit_repeat[i])
+    printf("Digit:      ");
+    while (i<10)
     {
-        /* code */
-        printf("%d\n",i);
-    }else{
-        m++;
-       
+        printf("%5d",digit_num[i]);
+        i++;
     }
-    i++;
-
-  }
-  if (m==10)
-  {
-      /* code */
-      printf("NONE");
-  }
+    printf("\n");
+    printf("Occurrences:");
+    while (m<10)
+    {
+        printf("%5d",digit_repeat[m]);
+        m++;
+    }
+  
+    while (t<10)
+    {
+        digit_repeat[t]=0;
+        t++;
+    }
+    t=0;
+    i=0;
+    m=0;
+    printf("\n");
+    }
   
     
     return 0;
