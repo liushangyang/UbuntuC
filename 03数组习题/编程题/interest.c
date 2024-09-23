9c900b0047fb4cfce2f16c8e1323959b1fe0310e
liushangyang <liu3469449669@hotmail.com>
2024-09-23 23:09:54 +0800

    Try New

--- 03数组习题/编程题/interest.c
+++ 03数组习题/编程题/interest.c
@@ -6,14 +6,19 @@
 #define BASE_MONEY 100.0
 
 int main(void){
-    int i,low_rate,mum_years,year;
+    int i,low_rate,mum_years,year=1000,month;
     double values[5];
+ 
+    //because i don't know Dynamic storage Share so it's ont prefct
     
     printf("Please input rate：");
     scanf("%d",&low_rate);
     printf("\n");
     printf("Enter years: ");
     scanf("%d",&mum_years);
+
+    double month_values[MUN_PATES][12][year];
+    double value_year[i][year];
 
     printf("\nYears");
     for ( i = 0; i < MUN_PATES ; i++)
@@ -22,18 +27,49 @@ int main(void){
         values[i]=BASE_MONEY;
     }
     printf("\n");
+    
 
-    for (year=1;year<=mum_years;year++)
+    for (year=0;year<mum_years;year++)
     {
-        printf("%3d   ",year);
+        printf("%3d   ",year+1);
         for ( i = 0; i < MUN_PATES; i++)
         {
             values[i]+=(i+low_rate)/100.0*values[i];
+            value_year[i][year]=values[i];
             printf("%7.2f",values[i]);
         }
         printf("\n");
         
     }
+    //builder : SunCanHelpU
+    for ( i = 0; i < MUN_PATES; i++)
+    {
+        for (month=0; month<12;month++)
+        {   
+            for(year=0;year<12;year++){
+            month_values[i][month][year]=value_year[i][year]*(i+low_rate)/100.0/12.0+value_year[i][year];
+            }
+            //We need a new group
+
+        }
+        
+    }
+
+    //builder : SunCanHelpU Chang : Make it possible to output month in years;
+    for (year=0;year<12;year++)
+    {
+        printf("%3d   ",year);
+        for(i=0;i<MUN_PATES;i++){
+            printf("rate:%2d   ",i+low_rate);
+            for(month=0;month<12;month++){
+                printf("%7.2f",month_values[i][month][year]);
+            }
+        }
+        printf("\n");
+    }
+    
+    
+    
     return 0;   
     
 }
